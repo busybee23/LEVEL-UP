3b8f119ddcafc5c0b01c31bf29ec93c2e240c7da
busybee23 <chaudharyyash088@gmail.com>
2022-09-09 09:25:55 -0700

    an important js script

---
+++ JULY-2022/new.js
@@ -0,0 +1,20 @@
+document.querySelector(".sqdOP.L3NKy.y3zKF").click()
+items[4].querySelector('button').click()
+
+var items = document.querySelectorAll(".follow, .js-follow, .btn, .btn-primary");
+for (var i = 0; i < items.length; i++) {
+    if (items[i].getAttribute("data-following") === "0") {
+        items[i].click();
+    }
+}
+
+var items = document.querySelectorAll("._acan._acap._acas")
+for(var i=3;i<items.length;i++){
+items[i].querySelector('button').click()
+}
+
+
+var items = document.querySelectorAll("._ab8w._ab94._ab97._ab9h._ab9k._ab9p._abb0._abcm")
+for(var i=10;i<items.length;i++){
+    items[i].querySelector('._acan._acap._acas').click()
+}
